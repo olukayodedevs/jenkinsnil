490084e3d70467ae5eea2f5404cbbaba56f78f3e
olukayodedevs <kellapaw@gmail.com>
2024-08-26 14:25:22 -0400

    rmvl 0f a str1ng

--- jenkinsnil.cpp
+++ jenkinsnil.cpp
@@ -12,7 +12,6 @@ int main()
         "Run", 
         "from", 
         "OlukayodeDevs", 
-        "and the C++ extension!",
         "",
         "LinkedIn: https://www.linkedin.com/in/arindeolukayode/",
         "Email: arindeolukayode@gmail.com"
